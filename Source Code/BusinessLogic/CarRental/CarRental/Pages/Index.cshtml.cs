e8ef09bba629510ca2a9fc66a30554a1a994cfd8
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2019-08-12 21:50:30 +0200

    Created CarController. Index and Car razor pages
    Co-Authored-By: dunaidevelop <dunaidevelop@users.noreply.github.com>
    Co-Authored-By: Rares Bunea <raresblea@users.noreply.github.com>
    Co-Authored-By: dragsirbu <dragsirbu@users.noreply.github.com>

--- Source Code/BusinessLogic/CarRental/CarRental/Pages/Index.cshtml.cs
+++ Source Code/BusinessLogic/CarRental/CarRental/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -13,5 +14,12 @@ namespace CarRental.Pages
         {
 
         }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; } = DateTime.Today;
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; } = DateTime.Today;
+
+
     }
 }
