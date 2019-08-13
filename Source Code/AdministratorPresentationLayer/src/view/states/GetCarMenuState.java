da5483d96c307dbcb489c0c5c1d082cfb692c511
dunaiDevelop <53299671+dunaiDevelop@users.noreply.github.com>
2019-08-13 14:24:32 +0200

    HTTP requests
    Co-Authored-By: dragsirbu <dragsirbu@users.noreply.github.com>
    Co-Authored-By: Rares Bunea <raresblea@users.noreply.github.com>
    Co-Authored-By: Vladimir Rotaru <senker@users.noreply.github.com>

--- Source Code/AdministratorPresentationLayer/src/view/states/GetCarMenuState.java
+++ Source Code/AdministratorPresentationLayer/src/view/states/GetCarMenuState.java
@@ -1,36 +1,46 @@
 package view.states;
 
+import HTTP_Requests.HttpURLConnectionExample;
 import Model.Car;
 
 public class GetCarMenuState extends CarMenuState {
     @Override
     public String handle(String input) {
-        switch (input) {
-            case "back":
-                currentState = states.get(1);
-                return "";
-            case "all":
-                StringBuilder allCarsInfoStringBuilder = new StringBuilder();
-                cars.forEach((car) -> allCarsInfoStringBuilder.append("\n").append(car.toString()));
-                return allCarsInfoStringBuilder.toString();
-        }
+        try {
+			switch (input) {
+			    case "back":
+			        currentState = states.get(1);
+			        return "";
+			    case "all":
+			        StringBuilder allCarsInfoStringBuilder = new StringBuilder();
+			        cars.forEach((car) -> allCarsInfoStringBuilder.append("\n").append(car.toString()));
+			        return allCarsInfoStringBuilder.toString();
+			    case "req":
+			    	HttpURLConnectionExample http = new HttpURLConnectionExample();
+			    	return http.getCarById("1").getCarBrand();
+				
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         Car carSearchResult = findCar(input);
 
         if(carSearchResult == null)
-            return "No car with such plates is registered";
+            return "No car with such registration number is registered";
         else
-            return carSearchResult.toString();
+            return carSearchResult.getCarBrand() + " " + carSearchResult.getCarModel();
     }
 
     @Override
     public String init() {
-        return "Enter car plates or enter 'all' to print information about all registered cars";
+        return "Enter car registration number or enter 'all' to print information about all registered cars";
     }
 
     private Car findCar(String plates) {
         for (Car car: cars) {
-            if (car.plates.equals(plates))
+            if (car.getRegNumber().equals(plates))
                 return car;
         }
 
